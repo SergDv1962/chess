f7b3207a88cabd1438188b600054223f81175920
SergDv1962 <dsv19621227@gmail.com>
2024-07-15 15:00:00 +0300

    add new board and figures

---
+++ src/models/Cell.ts
@@ -0,0 +1,23 @@
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figure/Figure";
+
+export class Cell {
+   readonly x: number;
+   readonly y: number;
+   readonly color: Colors;
+   figure: Figure | null;
+   board: Board;  // кільцева(рекурсивна) залежність між класами Board та Cell, це не дуже добре но автор це прийняв
+   available: boolean; // Можеш чи ні ходити на цю комірку
+   id: number; //Для реакт ключів
+
+   constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+      this.x = x;
+      this.y = y;
+      this.color = color;
+      this.figure = figure;
+      this.board = board;
+      this.available = false;
+      this.id = Math.random();
+   }
+}
