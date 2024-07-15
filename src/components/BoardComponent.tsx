e95a00d7f09aded7befa8c8c3c6ac028e4f75934
SergDv1962 <dsv19621227@gmail.com>
2024-07-15 17:32:21 +0300

    доступність комірок продовження

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Board } from '../models/Board';
 import CellComponent from './CellComponent';
 import { Cell } from '../models/Cell';
@@ -16,6 +16,21 @@ const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
       setSelectedCell(cell)
     }
   }
+
+  useEffect(() => {
+    highLightCells()
+  },[selectedCell])
+
+  function highLightCells() {
+    board.highLightCells(selectedCell)
+    updateBoard()
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard()
+    setBoard(newBoard)
+  }
+
   return (
     <div className='board'>
      {board.cells.map((row, index) => 
