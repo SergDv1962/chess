c72172e467441c3942db9e903f785391f3658709
SergDv1962 <dsv19621227@gmail.com>
2024-07-16 20:53:41 +0300

    Оформлюємо клас граючих. Вибір обо хід білого або черного. початок

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,40 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import BoardComponent from "./components/BoardComponent";
 import { Board } from "./models/Board";
+import { Colors } from "./models/Colors";
+import { Player } from "./models/Player";
 
 function App() {
   const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
     restart();
+    setCurrentPlayer(whitePlayer); // При старті починають білі
   }, []);
 
   function restart() {
     const newBoard = new Board();
     newBoard.initCells();
-    newBoard.addFigures()
+    newBoard.addFigures();
     setBoard(newBoard);
   }
+  // функція перемикача між граючими білі\чорні
+  function swapPlayer() {
+    setCurrentPlayer(
+      currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer
+    );
+  }
+
   return (
     <div className="app">
-      <BoardComponent 
-        board={board} 
-        setBoard={setBoard} 
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
       />
     </div>
   );
--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,57 +1,72 @@
-import React, { FC, useEffect, useState } from 'react'
-import { Board } from '../models/Board';
-import CellComponent from './CellComponent';
-import { Cell } from '../models/Cell';
+import React, { FC, useEffect, useState } from "react";
+import { Board } from "../models/Board";
+import CellComponent from "./CellComponent";
+import { Cell } from "../models/Cell";
+import { Player } from "../models/Player";
 
 interface BoardProps {
-   board: Board;
-   setBoard: (board: Board) => void;
+  board: Board;
+  setBoard: (board: Board) => void;
+  currentPlayer: Player | null;
+  swapPlayer: () => void;
 }
 
-const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
-  const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+const BoardComponent: FC<BoardProps> = ({
+  board,
+  setBoard,
+  currentPlayer,
+  swapPlayer,
+}) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
 
-  function click(cell:Cell) {
-    if(selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+  function click(cell: Cell) {
+    if (
+      selectedCell &&
+      selectedCell !== cell &&
+      selectedCell.figure?.canMove(cell)
+    ) {
       selectedCell.moveFigure(cell);
       setSelectedCell(null);
-      updateBoard()
-    }else{
-      setSelectedCell(cell)
+      updateBoard();
+    } else {
+      if (cell.figure?.color === currentPlayer?.color) { // умова щоб ми не могли віділяти ворожі фігури
+        setSelectedCell(cell);
+      }
     }
-
   }
 
   useEffect(() => {
-    highLightCells()
-  },[selectedCell])
+    highLightCells();
+  }, [selectedCell]);
 
   function highLightCells() {
-    board.highLightCells(selectedCell)
-    updateBoard()
+    board.highLightCells(selectedCell);
+    updateBoard();
   }
 
   function updateBoard() {
-    const newBoard = board.getCopyBoard()
-    setBoard(newBoard)
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
   }
 
   return (
-    <div className='board'>
-     {board.cells.map((row, index) => 
-      <React.Fragment key={index}>
-        {row.map(cell => 
-          <CellComponent
-            cell={cell}
-            click={click}
-            key={cell.id}
-            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
-          />
-        )}
-      </React.Fragment>
-     )}
+    <div className="board">
+      {board.cells.map((row, index) => (
+        <React.Fragment key={index}>
+          {row.map((cell) => (
+            <CellComponent
+              cell={cell}
+              click={click}
+              key={cell.id}
+              selected={
+                cell.x === selectedCell?.x && cell.y === selectedCell?.y
+              }
+            />
+          ))}
+        </React.Fragment>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default BoardComponent
+export default BoardComponent;
